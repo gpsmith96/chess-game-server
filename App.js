5b1aabf2043afedfa0187bbed94d4bb3f3b72788
gpsmith96 <48976136+gpsmith96@users.noreply.github.com>
2023-03-04 00:06:30 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,39 @@
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected!');
+  socket.send(socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+
+  socket.on('close', () => {
+    console.log('Client has disconnected!');
+    });
+
+  socket.on("private message", (anotherSocketId, msg) => {
+    console.log("Private message from " + socket.id +  " to " + anotherSocketId + ": " + msg);
+    socket.to(anotherSocketId).emit("private message", socket.id, msg);
+  });
+
+  socket.on("join room", (roomId) => {
+    console.log("Request from: " + socket.id + " to join room Id " + roomId);
+    socket.join(roomId);
+    io.to(roomId).emit("message", socket.id + " has joined the room!");
+  });
+
+  socket.on("broadcast", (roomId, msg) => {
+    console.log("broadcast from: " + socket.id + " to room Id " + roomId);
+    io.to(roomId).emit("message", msg);
+  });
+});
+
+httpServer.listen(8000);
+console.log("Now listening on port 8000");
